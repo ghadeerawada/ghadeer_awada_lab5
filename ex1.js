1f602096191794760dd817099995dde840673ec2
ghadeerawada <71120573+ghadeerawada@users.noreply.github.com>
2020-10-09 11:37:48 +0300

    Create ex1.js

---
+++ ex1.js
@@ -0,0 +1,11 @@
+function changeDay(){
+    let arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    for(let i=0; i< arr.length; i++){
+
+        document.querySelector('h1').innerHTML=arr[i];
+
+    }
+}
+
+    
